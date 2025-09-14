8a2fddb971730b04c5750a4ddc1e15caeec2f753
johnykosmos <piotrpomaranski97@gmail.com>
2025-09-14 14:17:43 +0200

    Feat: Add video removal on switching off

--- background.js
+++ background.js
@@ -20,6 +20,13 @@ browser.runtime.onMessage.addListener((message, _) => {
                 browser.storage.local.set({videos: videos});
             });
             break;
+        case "REMOVE_VIDEO":
+            browser.storage.local.get("videos").then((data) => {
+                const videos = data.videos || {};
+                delete videos[message.id]; 
+                browser.storage.local.set({videos: videos});
+            });
+            break;
     } 
 });
 
